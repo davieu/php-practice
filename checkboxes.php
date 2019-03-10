4f8a44cdc8676b37634024e28d401d674fbff60b
davieu <davisumana@gmail.com>
2019-03-10 13:08:16 -0400

    checkbox array

--- checkboxes.php
+++ checkboxes.php
@@ -6,9 +6,21 @@
 </head>
 <body>
 <!-- /code/php/php-practice/ -->
-  <form action="checkbox.php" method="post">
-    <input type="checkbox" name="fruits[]">
+  <form action="checkboxes.php" method="post">
+    Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
+    Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
+    Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
     <input type="submit">
   </form>
+
+  <?php
+    $fruits = $_POST["fruits"];
+    echo $fruits[0];
+    echo $fruits[1];
+    echo $fruits[2];
+    for ($i = 0; $i < count($fruits);$i++) {
+      echo "$fruits[$i] <br>";
+    };
+  ?>
 </body>
 </html>
