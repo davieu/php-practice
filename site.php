8725b9ab022560bfcf880967c3d4bccb000b6ce4
davieu <davisumana@gmail.com>
2019-03-10 00:55:40 -0500

    test

---
+++ site.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <meta charset="utf-8">
+    <title>PHP Demo</title>
+</head>
+<body>
+<!-- /code/php/php-practice/ -->
+	<?php
+		echo("Hello world");
+		echo "<h1>Davis' Site</h1>";
+		echo "<hr>";
+		echo "<p>This is my site</p>";
+	?>
+</body>
+</html>
