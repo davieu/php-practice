2393a2c2591bc687ec319735c4424b04139c220d
davieu <davisumana@gmail.com>
2019-03-11 15:28:12 -0400

    loops

--- while-loop.php
+++ while-loop.php
@@ -12,6 +12,26 @@
       echo "$index <br>";
       $index++;
     }
+    echo "<br>";
+
+
+    $num = 6;
+    do {
+      echo "$num <br>";
+      $num++;
+    } while ($num <= 5);
+    echo "<br>";
+
+    for ($i = 0; $i <= 5; $i++) {
+      echo $i;
+    }
+    echo "<br>";
+
+    $numbers = array(4, 8, 14, 23, 42);
+    for ($i = 0; $i <  count($numbers); $i++) {
+      echo $numbers[$i];
+    }
+
   ?>
 </body>
 </html>
