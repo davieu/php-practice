21acd6447eccfad8543c009448076ff43e01c99d
davieu <davisumana@gmail.com>
2019-03-12 01:41:13 -0400

    inheritance

--- 21Inheritance.php
+++ 21Inheritance.php
@@ -6,6 +6,40 @@
 </head>
 <body>
 <!-- /code/php/php-practice/ -->
+  <?php
+    class Chef {
+      function makeChicken() {
+        echo "The chef makes chicken <br>";
+      }
+
+      function makeSalad() {
+        echo "The chef makes salad <br>";
+      }
+
+      function makeSpecialDish() {
+        echo "The chef makes bbq ribs<br>";
+      }
+    }
+
+    class ItalianChef extends Chef{
+      function makePasta() {
+        echo "The chef makes pasta <br>";
+      }
+      //overriding the makeSpecialDish() function but only for the italian chef.
+      function makeSpecialDish() {
+        echo "The chef makes chicken parm <br>";
+      }
+    }
+
+    $chef = new Chef();
+    $chef->makeChicken();
+    $chef->makeSpecialDish();
+
+    $italianChef = new ItalianChef();
+    $italianChef->makeChicken();
+    $italianChef->makePasta();
+    $italianChef->makeSpecialDish();
+  ?>
 
 </body>
 </html>
